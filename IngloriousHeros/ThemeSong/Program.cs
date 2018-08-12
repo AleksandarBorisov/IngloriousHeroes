81c84ecbb520b01ce92bb334a21a0199f997c1e8
AleksandarBorisov <a_borisov@abv.bg>
2018-08-13 02:43:57 +0300

    Adding theme music

---
+++ IngloriousHeros/ThemeSong/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ThemeSong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(@"../../../../ThemeSong/bensound-happyrock.wav");
+            player.PlaySync();
+        }
+    }
+}
