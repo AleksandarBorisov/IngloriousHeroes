5f2b04ae5fb2ca6a84f8c3eb910ef538c0890222
AleksandarBorisov <a_borisov@abv.bg>
2018-08-25 19:39:28 +0300

    Added Draw Caption Module

---
+++ IngloriousHeros/IngloriousHeros/Core/UI/DrawCaption/DrawCaptionBlinking.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using IngloriousHeros.Core.UI.DrawCaption.Interfaces;
+
+//namespace IngloriousHeros.Core.UI.DrawCommands
+//{
+//    class DrawCaptionBlinking : IDrawCaption
+//    {
+//        public DrawCaptionBlinking()
+//        {
+//        }
+
+//        public void Execute(List<string> parameters)
+//        {
+//            throw new NotImplementedException();
+//        }
+//    }
+//}
